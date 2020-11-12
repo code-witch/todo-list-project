22efe1e860d03c0d507027b02ac6a4985e7799d1
code-witch <code-witch@users.noreply.github.com>
2020-11-12 10:43:18 -0700

    basic server

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const routes = require('./routes/routes');
+const bodyParser = require('body-parser');
+
+const hostname = 'localhost';
+const port = '3000';
+
+const urlEncodedParser = bodyParser.urlencoded({
+    extended: true
+});
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', routes.index);
+
+app.listen(port, hostname, () =>{
+    console.log(`todo-list running at http://${hostname}:${port}`);
+});
