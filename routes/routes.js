d7a0770fe33c1a6d50b3725100ce7cd60c50a6a6
code-witch <code-witch@users.noreply.github.com>
2020-11-17 10:15:50 -0700

    json writing data

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,24 @@ const index = (req,res) => {
     res.render('index')
 }
 
+// return all data
+const getAllData = () => {
+    let data = JSON.parse(fs.readFileSync('./data.json', {encoding:'utf-8'}));
+    return data;
+}
+
+// add user to the data file
+const addTask = task => {
+    let data = getAllData();
+    data.push(task);
+    writeAllData(data);
+};
+
+// override all data with new data
+const writeAllData = data => {
+    fs.writeFileSync('./data.json',JSON.stringify(data, null, 4),{encoding:'utf-8'});
+}
+
 
 module.exports = {
     index,
