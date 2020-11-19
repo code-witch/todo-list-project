9d8d4401e3addb108d45940773e8478da65f82fc
code-witch <code-witch@users.noreply.github.com>
2020-11-19 12:54:27 -0700

    fix some stuff

--- routes/routes.js
+++ routes/routes.js
@@ -1,39 +1,66 @@
 const fs = require("fs");
 
-exports.index = (req, res) => {
+ const index = (req, res) => {
     if (!fs.existsSync('tasks.json')) {
-        fs.writeFileSync("tasks.json", "{\"tasks\":[]}");
+        fs.writeFileSync("tasks.json", "[]");
     }
-    let tasks = JSON.parse(fs.readFileSync('./tasks.json'))
+    // let tasks = JSON.parse(fs.readFileSync('./tasks.json'))
 
     res.render("index", {
         "title": "Index",
-        "tasks": tasks.tasks
+        "tasks": getAllData()
     });
 };
 
-exports.createTask = (req, res) => {
+const createTask = (req, res) => {
     let task = {
         "taskName": req.body.name,
         "taskDesc": req.body.desc
     };
-    readFile(task);
+    addTask(task);
     res.redirect('/');
 };
 
-const readFile = (task) => {
+// const readFile = (task) => {
 
-    if (!fs.existsSync('tasks.json')) {
-        fs.writeFileSync("tasks.json", "{\"tasks\":[]}");
-    }
-    fs.readFile("tasks.json", { encoding: 'utf-8' }, function (err, tasks) {
-        if (err) throw err;
-        var arrayTasks = JSON.parse(tasks);
-        arrayTasks.tasks.push(task);
-        // console.log(arrayTasks);
+//     if (!fs.existsSync('tasks.json')) {
+//         fs.writeFileSync("tasks.json", "{\"tasks\":[]}");
+//     }
+//     fs.readFile("tasks.json", { encoding: 'utf-8' }, function (err, tasks) {
+//         if (err) throw err;
+//         var arrayTasks = JSON.parse(tasks);
+//         arrayTasks.tasks.push(task);
+//         // console.log(arrayTasks);
 
-        fs.writeFile("tasks.json", JSON.stringify(arrayTasks), "utf-8", function () {
-            console.log("Write Done");
-        });
-    });
+//         fs.writeFile("tasks.json", JSON.stringify(arrayTasks), "utf-8", function () {
+//             console.log("Write Done");
+//         });
+//     });
+// }
+
+
+
+
+// return all data
+const getAllData = () => {
+    let data = JSON.parse(fs.readFileSync('./tasks.json', {encoding:'utf-8'}));
+    return data;
+}
+
+// add user to the data file
+const addTask = task => {
+    let data = getAllData();
+    data.push(task);
+    writeAllData(data);
+};
+
+// override all data with new data
+const writeAllData = data => {
+    fs.writeFileSync('./tasks.json',JSON.stringify(data, null, 4),{encoding:'utf-8'});
+}
+
+
+module.exports = {
+    index,
+    createTask
 }
